1a40a8e43e5d0777d9c5d65969d6c1392a7568e7
Ivan <ivan.perazzini@gmail.com>
2020-05-23 10:54:16 +0200

    update paths, add singular token check in main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
-import {PageStatusService} from './page-status.service';
+import { Component, OnInit } from '@angular/core';
+import {PageStatusService} from './services/page-status.service';
+import { TokenService } from "@services/token.service"
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cassanova-web-menu';
+  private tokenSub : Observable<any>;
 
-  constructor(public page: PageStatusService) { }
+  constructor(public page: PageStatusService,
+              private token: TokenService ) { }
+  
+  //TODO: remove this method, substitute with guard
+  ngOnInit(): void {
+    this.tokenSub = this.token.loadToken();
+  }
 
   public isPage(page: string): boolean { return this.page.isPage(page); }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { ProductsComponent } from './products/products.component';
+import { CategoriesComponent } from './compoenents/categories/categories.component';
+import { ProductsComponent } from './compoenents/products/products.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import {TokenInterceptor } from './token-interceptor';
+import {TokenInterceptor } from './services/token-interceptor';
 import { NoHTMLPipe } from './no-html.pipe';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
@@ -17,13 +17,13 @@ import {
   MatListModule, MatProgressSpinnerModule,
   MatToolbarModule,
 } from '@angular/material';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductDetailComponent } from './compoenents/product-detail/product-detail.component';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
 import {ScrollingModule} from '@angular/cdk/scrolling';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { SearchProductsComponent } from './search-products/search-products.component';
+import { PageNotFoundComponent } from './compoenents/page-not-found/page-not-found.component';
+import { SearchProductsComponent } from './compoenents/search-products/search-products.component';
 import {FormsModule} from '@angular/forms';
-import { SalesPointComponent } from './sales-point/sales-point.component';
+import { SalesPointComponent } from './compoenents/sales-point/sales-point.component';
 
 @NgModule({
   declarations: [
