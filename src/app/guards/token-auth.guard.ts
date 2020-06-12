15e8205537b7c606a922c67aa92388f3a6993896
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-06-12 10:02:06 +0200

    Add angular authguard

---
+++ src/app/guards/token-auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivateChild, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenService } from '@services/token.service'
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenAuthGuard implements CanActivate, CanActivateChild {
+
+  constructor(private token: TokenService, private router: Router) {}
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+    return this.canActivate(childRoute, state);
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree>
+  {
+    return this.token.loadToken().pipe(
+      map(res => res === false ? this.router.parseUrl('/401') : res)
+    )
+  }
+
+  
+}
