4030a4205ec2d1b80a4d121cf37388bf1f83e5f7
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-06-12 09:51:46 +0200

    Change nav bar component. Now it's instantiated in multiple components, thus it is a routed element

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PageType, PageStatusService } from '@app/services/page-status.service';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
@@ -15,20 +16,11 @@ export class NavBarComponent implements OnInit {
   private idSp : string;
 
   constructor(private pageInfo : PageStatusService,
-              private router : Router) 
-  { 
-    this.pageType = pageInfo.getpageType();
-    router.events.pipe(
-      filter(e => e instanceof NavigationEnd)
-    ).subscribe(e => {
-      this.pageType = pageInfo.getpageType();
-      if(this.pageType !== PageType.SALESPOINT) {
-        this.idSp = pageInfo.getTokenizedUrl()[1];
-      }
-    });
-  }
-
+              private route : ActivatedRoute) { }
+  
   ngOnInit(): void {
+    this.pageType = this.pageInfo.getpageType();
+    this.idSp = this.route.snapshot.paramMap.get("idSp");
   }
 
 }
