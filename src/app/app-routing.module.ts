64956d1129ad29b46b6ad3236e63b7c921cb09a1
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-06-12 09:32:35 +0200

    Add child routes, default route and error pages to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,30 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CategoriesComponent} from '@components/categories/categories.component';
 import {ProductsComponent} from '@components/products/products.component';
-import {PageNotFoundComponent} from '@components/page-not-found/page-not-found.component';
+import {PageNotFoundComponent} from '@app/components/messages/page-not-found/page-not-found.component';
 import {SearchProductsComponent} from '@components/search-products/search-products.component';
 import {SalesPointComponent} from '@components/sales-point/sales-point.component';
+import { TokenAuthGuard } from './guards/token-auth.guard';
+import { AuthFailedComponent } from './components/messages/auth-failed/auth-failed.component';
+import { ServerErrorComponent } from './components/messages/server-error/server-error.component';
 
 const routes: Routes = [
-  { path: ':idSp/categories', component: CategoriesComponent },
-  { path: ':idSp/categories/:id', component: ProductsComponent },
-  { path: ':idSp/search', component: SearchProductsComponent },
-  { path: 'salespoint', component: SalesPointComponent},
-  { path: 'error', component: PageNotFoundComponent},
-  { path: '**', redirectTo: 'error' }
-
+  { path: '', redirectTo: 'salespoint', pathMatch: 'full'},
+  { path: '404', component: PageNotFoundComponent},
+  { path: '401', component: AuthFailedComponent},
+  { path: '500', component: ServerErrorComponent},
+  { path: 'salespoint', canActivate: [TokenAuthGuard], component: SalesPointComponent},
+  {
+    path: ':idSp',
+    canActivateChild: [TokenAuthGuard],
+    children: [
+      { path: 'categories', component: CategoriesComponent },
+      { path: 'categories/:id', component: ProductsComponent },
+      { path: 'search', component: SearchProductsComponent },
+      { path: '**', redirectTo: '/error'}
+    ]
+  },
+  { path: '**', redirectTo: '/error'}
 ];
 
 @NgModule({
