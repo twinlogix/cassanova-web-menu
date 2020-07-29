8bd7e17e3914a560adcbe543b905c12c88340e00
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-07-29 14:03:04 +0200

    First restyle of product details component

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -3,6 +3,7 @@ import { ProductService } from '@app/services/product.service';
 import { Observable } from 'rxjs';
 import { Product } from '@app/classes/Product';
 import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-detail',
@@ -18,7 +19,15 @@ export class ProductDetailComponent implements OnInit {
 
   ngOnInit() {
     const id : string = this.route.snapshot.paramMap.get("id");
-    this.productSub = this.productService.getProducts({ids : [id]});
+    this.productSub = this.productService.getProducts({ids : [id]}).pipe(
+      map(res => res.map(prod => this.prepareProduct(prod)))
+    );
+  }
+
+  private prepareProduct(prod : Product) : Product {
+    let res = prod;
+    res.descriptionExtended = res.descriptionExtended ?? "Nessuna descrizione disponibile."
+    return res;
   }
 
 }
