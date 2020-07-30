d449868a5cc8314e5cb9e0d42f115b83edc32974
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-07-30 16:26:26 +0200

    Change return value to response message code

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, VirtualTimeScheduler} from 'rxjs';
 import {catchError} from 'rxjs/internal/operators/catchError';
 import {tap} from 'rxjs/internal/operators/tap';
 import {of} from 'rxjs/internal/observable/of';
@@ -16,26 +16,28 @@ export class TokenService {
   private token: string;
   private lastApiKey: string;
   private lastDate: number = Date.now();
-  private requestUrl = `${this.httpUtils.getHostname()}/apikey/token`;
-  private TOKEN_VALIDITY_MS = 3600000;
-
+  private requestUrl : string = `${this.httpUtils.getHostname()}/apikey/token`;
+  private tokenValidityMs : number = 0;
+  private OK_CODE : number = 200;
   constructor(private http: HttpClient, private httpUtils: HttpUtilsService, private page: PageStatusService) {}
 
-  public loadToken(): Observable<boolean> {
+  public loadToken(): Observable<number> {
     const currApiKey = 'ebf79958-415e-4333-b746-7b3375802fa7'//apiKey;
     const currDate = Date.now();
-    if (this.token === undefined || this.lastApiKey.localeCompare(currApiKey) !== 0 || currDate - this.lastDate >= this.TOKEN_VALIDITY_MS) { // Load token
+    if (this.token === undefined || this.lastApiKey.localeCompare(currApiKey) !== 0 || currDate - this.lastDate >= this.tokenValidityMs) { // Load token
       return this.http.post(this.requestUrl, JSON.stringify(this.getBody(currApiKey)), this.httpUtils.getTokenHttpOptions()).pipe(
         tap(response => {
+          console.log(response)
+          this.tokenValidityMs = response["expires_in"] * 1000;
           this.token = response["access_token"];
           this.lastApiKey = currApiKey;
           this.lastDate = currDate;
         }),
-        map(res => true),
-        catchError(this.httpUtils.handleError('token', false)), // then handle the error
+        map(res => this.OK_CODE),
+        catchError(this.httpUtils.handleErrorErrCode('token')), // then handle the error
       );
     } else {
-      return of(true);
+      return of(this.OK_CODE);
     }
   }
 
