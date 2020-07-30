0c8cff12915ae24e585bb6fd4504b58d805be71f
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-07-30 09:28:41 +0200

    Remove loading template, add overflow handling

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import { ProductService } from '@app/services/product.service';
 import { Observable } from 'rxjs';
 import { Product } from '@app/classes/Product';
@@ -10,14 +10,20 @@ import { map } from 'rxjs/operators';
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnInit, OnDestroy{
 
   private productSub : Observable<Product[]>;
 
   constructor(private productService : ProductService,
               private route : ActivatedRoute) { }
 
+  //TODO: gestisci in modo decente overflow di body
+  ngOnDestroy(): void {
+    document.body.classList.replace("overflow-visible", "overflow-hidden");
+  }
+
   ngOnInit() {
+    document.body.classList.replace("overflow-hidden", "overflow-visible");
     const id : string = this.route.snapshot.paramMap.get("id");
     this.productSub = this.productService.getProducts({ids : [id]}).pipe(
       map(res => res.map(prod => this.prepareProduct(prod)))
