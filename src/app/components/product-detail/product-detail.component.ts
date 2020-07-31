e3e025613e4f4289b36ab63a7ae0f57b0bc82cb9
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-07-31 10:56:22 +0200

    Add MaterialSelect for quantity selection

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { Product } from '@app/classes/Product';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/operators';
+import { StockService } from '@app/services/stock.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -12,22 +13,28 @@ import { map } from 'rxjs/operators';
 })
 export class ProductDetailComponent implements OnInit, OnDestroy{
 
-  private productSub : Observable<Product[]>;
-
+  private productSub  : Observable<Product[]>;
+  private quantitySub : Observable<number[]>;
+  private selectedQuantity : number = 1;
   constructor(private productService : ProductService,
-              private route : ActivatedRoute) { }
+              private stockService   : StockService,
+              private route          : ActivatedRoute) { }
 
-  //TODO: gestisci in modo decente overflow di body
+  //TODO: vedi se riesci a gestire meglio overflow di body
   ngOnDestroy(): void {
     document.body.classList.replace("overflow-visible", "overflow-hidden");
   }
 
   ngOnInit() {
     document.body.classList.replace("overflow-hidden", "overflow-visible");
-    const id : string = this.route.snapshot.paramMap.get("id");
-    this.productSub = this.productService.getProducts({ids : [id]}).pipe(
+    const idSp : number = Number.parseInt(this.route.snapshot.paramMap.get("idSp"));
+    const idProd : string = this.route.snapshot.paramMap.get("id");
+    this.productSub = this.productService.getProducts({ids : [idProd]}).pipe(
       map(res => res.map(prod => this.prepareProduct(prod)))
     );
+    this.quantitySub = this.stockService.getStock({idProduct: [idProd]}, idSp).pipe(
+      map(res => Array.from(Array(res[0].quantity), (_, i) => i + 1))
+    )
   }
 
   private prepareProduct(prod : Product) : Product {
