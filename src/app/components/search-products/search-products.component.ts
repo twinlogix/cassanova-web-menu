52ee6f9a9fd29ee9d77abdead2294fd85b28d47f
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-08-06 09:54:16 +0200

    First overhaul of the search-products component

--- src/app/components/search-products/search-products.component.ts
+++ src/app/components/search-products/search-products.component.ts
@@ -1,83 +1,78 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Product} from '@classes/Product';
-import { Observable, Subscription} from 'rxjs';
-import { tap, map, filter } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { Product } from '@classes/Product';
+import { Observable } from 'rxjs';
 import { ProductService } from '@app/services/product.service';
 import { ActivatedRoute, Router, NavigationStart } from '@angular/router';
 import { Category } from '@app/classes/Category';
 import { CategoryService } from '@app/services/category.service';
-import { ProductDetailComponent } from '../product-detail/product-detail.component';
-import {ProductsRequest} from '@classes/QueryParams'
+import { ProductsRequest } from '@classes/QueryParams'
+import { InfiniteScrollableComponent } from '../infinite-scrollable/infinite-scrollable.component';
 
 @Component({
   selector: 'app-search-products',
   templateUrl: './search-products.component.html',
   styleUrls: ['./search-products.component.scss']
 })
-export class SearchProductsComponent implements OnInit, OnDestroy {
+export class SearchProductsComponent extends InfiniteScrollableComponent<Product> {
 
-  // Subscriptions
-  private resultsSub : Observable<Product[]>;
   private categoriesSub : Observable<Category[]>;
-  private routerSub : Subscription;
-
-  private disabled : boolean = false;
   private productName : string = "";
   private productDescription : string = "";
   private productCategory : string = undefined;
   private idSp : number;
+  private idCategory : string;
 
   constructor(
     private productService: ProductService,
     private categoryService: CategoryService,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
-
-  ngOnDestroy(): void {
-    if(this.routerSub) {
-      this.routerSub.unsubscribe();
-    }
+  ) { 
+    super(productService, prepareProduct, filterProducts)
   }
 
   ngOnInit() {
     this.idSp = parseInt(this.route.snapshot.paramMap.get('idSp'));
-    this.resultsSub = this.getProducts({idsSalesPoint : [this.idSp]});
-    this.categoriesSub = this.categoryService.getCategories({idsSalesPoint : [this.idSp], start : 0, limit : 100});  //TODO: remove magic number 100
-  }
-
-  private getProducts(params : ProductsRequest): Observable<Product[]> {
-    const defaultImageUrl : string = '/assets/default.png';
-    return this.productService.getProducts(params).pipe(
-      map(p => this.filterProducts(p)),
-      tap(prods => {
-        for(let p of prods) {
-          if(!p.images) {
-            p.images = [{imageUrl : defaultImageUrl}];
-          }
-          if(p.images.length == 0) {
-            p.images.push({imageUrl : defaultImageUrl});
-          }
-        }
-      }),
-    );
+    this.idCategory = this.route.snapshot.paramMap.get("idCat");
+    const query : ProductsRequest = {idsSalesPoint : [this.idSp]}
+    if(this.idCategory) {
+      query.idsCategory = [this.idCategory];
+    }
+    this.setQuery(query);
+    this.getItems();
+    this.categoriesSub = this.categoryService.getData({idsSalesPoint : [this.idSp], start : 0, limit : 100});  //TODO: remove magic number 100
   }
 
   private onSubmit(): void {
-    this.resultsSub = this.getProducts({ idsSalesPoint : [this.idSp], 
-                                         idsCategory : [this.productCategory],
-                                         start : 0, 
-                                         limit : 100}); //TODO: remove magic number 100
+    const query : ProductsRequest = {
+      idsSalesPoint : [this.idSp], 
+      idsCategory : [this.idCategory ?? this.productCategory],
+      start : 0, 
+      limit : 100
+    }
+    this.setQuery(query)
+    this.getItems(true);
   }
-
-  private filterProducts(products: Product[]): Product[] {
-    return products.filter(p => p.description.toLowerCase().includes(this.productName) &&
-                                this.checkDescr(p.descriptionExtended, this.productDescription));
-  }
-
-  private checkDescr(descr : string, filterDescr : string) : boolean {
-    const d : string = descr ?? "";
-    return d.toLocaleLowerCase().includes(filterDescr);
-  }
-
 }
+
+function prepareProduct(prod : Product) : Product {
+  const defaultImageUrl : string = '/assets/default.png';
+  let res = prod;
+  if(!res.images) {
+    res.images = [{imageUrl : defaultImageUrl}];
+  }
+  if(res.images.length == 0) {
+    res.images.push({imageUrl : defaultImageUrl});
+  }
+  return res;
+}
+
+function filterProducts(products: Product[]): Product[] {  
+  return products.filter(p => p.description.toLowerCase().includes(this.productName) &&
+                              checkDescr(p.descriptionExtended, this.productDescription));
+}
+
+function checkDescr(descr : string, filterDescr : string) : boolean {
+  const d : string = descr ?? "";
+  return d.toLocaleLowerCase().includes(filterDescr);
+};
