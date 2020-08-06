c887ee7e0d063c9018f920b8bc64cfda7e431f47
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-08-06 10:02:08 +0200

    Extend infinite scrollable component

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,57 +1,51 @@
-import {Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 import { Product } from '@classes/Product';
-import {ProductService} from '@services/product.service';
-import {ActivatedRoute, Router, NavigationStart} from '@angular/router';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Channel, ProductsRequest } from '@app/classes/QueryParams';
+import { ProductService } from '@services/product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Channel } from '@app/classes/QueryParams';
+import { InfiniteScrollableComponent } from '../infinite-scrollable/infinite-scrollable.component';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent extends InfiniteScrollableComponent<Product> {
+//Possibile implementazione a mano di virtual scrolling: ascolta evento ridimensionamento finestra e basati sul layout per definire quanti elementi vuoi nel buffer
 
-  private productsSub : Observable<Product[]>;
-  private disabled : boolean = false;
-
-  constructor(
-    private productService: ProductService,
-    private route: ActivatedRoute,
-    public router: Router
-  ) { }
+constructor(
+  private productService: ProductService,
+  private route: ActivatedRoute,
+  public router: Router
+  ) {
+    super(productService, prepareProduct)
+  }
 
   ngOnInit() {
-    this.productsSub = this.getProducts();
-  }
-
-  private getProducts(): Observable<Product[]> {
+    const start : number = 0;
+    const limit : number = 15;
     const categoryId : string = this.route.snapshot.paramMap.get('id');
     const idSp : number = parseInt(this.route.snapshot.paramMap.get('idSp'));
-    const query : ProductsRequest = {
-      idsSalesPoint: [idSp],
-      idsCategory: [categoryId],
-      enabledForChannels: [Channel.MOBILE_COMMERCE],
-      start: 0,
-      limit : 6/*TEMP*/
+    const query = {
+       idsSalesPoint: [idSp],
+       idsCategory: [categoryId],
+       enabledForChannels: [Channel.MOBILE_COMMERCE],
+       start : start,
+       limit : limit
     }
-
-    return this.productService.getProducts(query).pipe(
-      map(res => res.map(prod => this.prepareProduct(prod)))
-    );
+    this.setQuery(query);
+    this.getItems();
   }
+}
 
-  private prepareProduct(prod : Product) : Product {
-    const defaultImageUrl : string = '/assets/default.png';
-    let res = prod;
-    if(!res.images) {
-      res.images = [{imageUrl : defaultImageUrl}];
-    }
-    if(res.images.length == 0) {
-      res.images.push({imageUrl : defaultImageUrl});
-    }
-    return res;
+function prepareProduct(prod : Product) : Product {
+  const defaultImageUrl : string = '/assets/default.png';
+  let res = prod;
+  if(!res.images) {
+    res.images = [{imageUrl : defaultImageUrl}];
   }
-
+  if(res.images.length == 0) {
+    res.images.push({imageUrl : defaultImageUrl});
+  }
+  return res;
 }
