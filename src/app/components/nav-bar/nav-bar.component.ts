6406de2adc31ba015a96fda08b11ae1421125b74
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-08-06 10:13:08 +0200

    Add search button

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PageType, PageStatusService } from '@app/services/page-status.service';
 import { ActivatedRoute } from '@angular/router';
-import { map, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
 import { CategoryService } from '@app/services/category.service';
 import { ProductService } from '@app/services/product.service';
@@ -16,9 +16,11 @@ export class NavBarComponent implements OnInit {
   private pageType : PageType;
   private PageType = PageType;
   private barTitle : Observable<string>;
-  private returnRoute : string;
-  private icon : string;
-  private label : string;
+  private backRoute : string;
+  private backIcon : string;
+  private backLabel : string;
+  private searchLabel : string;
+  private searchRoute : string;
 
   constructor(private pageInfo : PageStatusService,
               private route : ActivatedRoute,
@@ -33,31 +35,41 @@ export class NavBarComponent implements OnInit {
         break;
       case PageType.CATEGORIES:
         this.barTitle = of("Categorie");
-        this.returnRoute = "/salespoint";
-        this.label = "Torna ai punti vendita";
-        this.icon = "store";
+        this.backRoute = "/salespoint";
+        this.backLabel = "Torna ai punti vendita";
+        this.backIcon = "store";
+        this.searchLabel = "Vai alla pagina di ricerca";
+        this.searchRoute = "search";
         break;
       case PageType.PRODUCTS:
         const idCat : string = this.route.snapshot.paramMap.get("id");
         //The category id is unknown when the navigation is still pending. The page is also refreshed when the authentication ends, so this check prevents
         //querying the server two times
         if(idCat) {
-          this.returnRoute = "..";
-          this.label = "Torna alla categoria";
-          this.icon = "keyboard_arrow_left";
-          this.barTitle = this.categoriesService.getCategories({ids : [idCat]}).pipe(
+          this.backRoute = "..";
+          this.backLabel = "Torna alla categoria";
+          this.backIcon = "keyboard_arrow_left";
+          this.barTitle = this.categoriesService.getData({ids : [idCat]}).pipe(
             map(res => res.length > 0 ? res[0].description : "")
           );
+          this.searchLabel = "Vai alla pagina di ricerca";
+          this.searchRoute = "search";
         }
+        break;
+      case PageType.SEARCH:
+        this.barTitle = of("Cerca");
+        this.backIcon = "keyboard_arrow_left";
+        this.backLabel = "Torna indietro";
+        this.backRoute = "../"
         break;
       case PageType.PRODUCT_DETAIL:
         {
           const prodId : string = this.route.snapshot.paramMap.get("id");
           if(prodId) {
-            this.returnRoute = "../..";
-            this.label = "Torna alla lista prodotti";
-            this.icon = "keyboard_arrow_left";
-            this.barTitle = this.productsService.getProducts({ids : [prodId]}).pipe(
+            this.backRoute = "../..";
+            this.backLabel = "Torna alla lista prodotti";
+            this.backIcon = "keyboard_arrow_left";
+            this.barTitle = this.productsService.getData({ids : [prodId]}).pipe(
               map(res => res.length > 0 ? res[0].description : "")
             );
           }
