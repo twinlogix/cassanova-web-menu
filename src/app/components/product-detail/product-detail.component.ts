cab75ec115d7389587044d652ab8be202d321c00
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-08-06 10:11:31 +0200

    Add additional checks to handle unauthorized access to Stock service. Fix item quantity appearence when the item had no items in stock

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -3,37 +3,36 @@ import { ProductService } from '@app/services/product.service';
 import { Observable } from 'rxjs';
 import { Product } from '@app/classes/Product';
 import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { StockService } from '@app/services/stock.service';
+import { Stock } from '@classes/Stock'
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
-export class ProductDetailComponent implements OnInit, OnDestroy{
+export class ProductDetailComponent implements OnInit {
 
   private productSub  : Observable<Product[]>;
   private quantitySub : Observable<number[]>;
   private selectedQuantity : number = 1;
+  //Due to permission restrictions to some accounts (apiKeys), some stores are not allowed to access the Stock server.
+  //Use this variable to hide all stock information in the template
+  private unauthorized : boolean = false;
   constructor(private productService : ProductService,
               private stockService   : StockService,
               private route          : ActivatedRoute) { }
 
-  //TODO: vedi se riesci a gestire meglio overflow di body
-  ngOnDestroy(): void {
-    document.body.classList.replace("overflow-visible", "overflow-hidden");
-  }
-
   ngOnInit() {
-    document.body.classList.replace("overflow-hidden", "overflow-visible");
     const idSp : number = Number.parseInt(this.route.snapshot.paramMap.get("idSp"));
     const idProd : string = this.route.snapshot.paramMap.get("id");
-    this.productSub = this.productService.getProducts({ids : [idProd]}).pipe(
+    this.productSub = this.productService.getData({ids : [idProd]}).pipe(
       map(res => res.map(prod => this.prepareProduct(prod)))
     );
     this.quantitySub = this.stockService.getStock({idProduct: [idProd]}, idSp).pipe(
-      map(res => Array.from(Array(res[0].quantity), (_, i) => i + 1))
+      tap(res => this.unauthorized = res.toString() === this.stockService.UNAUTHORIZED_MESSAGE),
+      map(res => res.length > 0 && res[0].quantity ? Array.from(Array(res[0].quantity), (_, i) => i + 1) : []),
     )
   }
 
