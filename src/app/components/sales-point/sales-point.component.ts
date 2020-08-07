5c7752fb4428bb3c2ab01ba041622387cd27622d
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-08-07 15:47:39 +0200

    Add formatting of salespoint address in template cards

--- src/app/components/sales-point/sales-point.component.ts
+++ src/app/components/sales-point/sales-point.component.ts
@@ -11,7 +11,6 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SalesPointComponent implements OnInit {
 
-  // Subscriptions
   private salesPointSub : Observable<SalesPoint[]>;
 
   constructor(
@@ -23,28 +22,18 @@ export class SalesPointComponent implements OnInit {
     this.salesPointSub = this.getSalesPoint();
   }
 
-  //TODO:
-  //getAddres(SalesPoint) method: call this from the html to format the address
-  //
-  // Obviously, you'll have to modify this:
-  //
-  // for (const salePoint of salesPoint) {
-  //   let address = '';
-  //   const street = salePoint.hasOwnProperty('street') ? salePoint.street : '';
-  //   let city = salePoint.hasOwnProperty('city') ? salePoint.city : '';
-  //   const country = salePoint.hasOwnProperty('country') ? salePoint.country : '';
-  //   city = city + (city !== '' && country !== '' ? ' - ' : '') + country;
-  //   address = street;
-  //   if (city !== '') {
-  //     address = address + (street !== '' ? ' ( ' : '') + city + (street !== '' ? ' )' : '');
-  //   }
-  //   const salePointItem = new SalesPoint(salePoint.id, salePoint.name, address);
-  //   this.salesPoint.push(salePointItem);
-  //     this.salesPointMap.set(salePoint.id, salePointItem);
-  //   }
-  //   this.loading = LOAD_FINISH;
-  // })
-
+  getAddress(sp : SalesPoint) : string {
+    let address = '';
+    const street = sp.street ?? '';
+    let city = sp.city ?? '';
+    const country = sp.country ?? '';
+    city = city + (city !== '' && country !== '' ? ' - ' : '') + country;
+    address = street;
+    if (city !== '') {
+      address = address + (street !== '' ? ' ( ' : '') + city + (street !== '' ? ' )' : '');
+    }
+    return address;
+  }
 
   private getSalesPoint(): Observable<SalesPoint[]> {
     return this.salesPointService.getData();
